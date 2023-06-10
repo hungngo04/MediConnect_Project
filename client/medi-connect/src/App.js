063dcf0fdc9fad4c381447fd90012b35a9e7596f
Hung Ngo <>
2023-06-10 17:17:01 +0700

    Test the api

--- client/medi-connect/src/App.js
+++ client/medi-connect/src/App.js
@@ -1,7 +1,13 @@
+import { Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import ChatPage from './pages/ChatPage/ChatPage';
+
+
 function App() {
   return (
     <div className="App">
-
+      <Route path='/' component={HomePage} exacts> HomePage </Route>
+      <Route path='/chats' component={ChatPage} exacts> ChatPage </Route>
     </div>
   );
 }
